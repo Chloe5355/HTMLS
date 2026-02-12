a602cf9b3c46f82739f0e58a5cf0594ef075afe5
Chloe5355 <sasekyou14@gmail.com>
2026-02-12 15:57:40 +0900

    player.js の更新

--- Memory/js/player.js
+++ Memory/js/player.js
@@ -1,21 +1,17 @@
-// player.js
 function playerTurn(card){
-  if(currentTurn !== 'player' || lockBoard) return;
+  if(currentTurn!=='player' || lockBoard) return;
+
   if(selected.includes(card) || card.classList.contains('matched')) return;
 
   card.textContent = card.dataset.icon;
-  card.classList.add('flip');
-  setTimeout(()=>card.classList.remove('flip'), 300);
   selected.push(card);
   rememberCard(card);
 
-  if(selected.length === 2) {
+  if(selected.length===2){
     lockBoard = true;
     setTimeout(()=>{
       checkMatch('player');
       lockBoard = false;
-    }, 1000);
+    },1000);
   }
-
-  updateScore();
 }
